412f5c1915b079b3a341f2cb433b9c7109e546e0
CinLo <cinlos3382@gmail.com>
2022-12-11 22:15:20 -0300

    ejercicios con this; en el proyecto agrgamos el boton mute/unmute

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,23 +1,39 @@
-
-
 function MediaPlayer(config) {
     this.media = config.el;
-}
+    this.plugins = config.plugins || [];
   
-MediaPlayer.prototype.play = function() {
-    this.media.play();  
-};
- 
-MediaPlayer.prototype.play = function() {
-    this.media.paused();  
-};
-    
-MediaPlayer.prototype.togglePlay = function() {
+    this._initPlugins();
+  }
+  
+  MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+      plugin.run(this);
+    });
+  };
+  
+  MediaPlayer.prototype.play = function() {
+    this.media.play();
+  };
+  
+  MediaPlayer.prototype.pause = function() {
+    this.media.pause();
+  };
+  
+  MediaPlayer.prototype.togglePlay = function() {
     if (this.media.paused) {
       this.play();
     } else {
-        this.pause();
+      this.pause();
     }
-};
-   
+  };
+  
+  MediaPlayer.prototype.mute = function() {
+    this.media.muted = true;
+  };
+  
+  MediaPlayer.prototype.unmute = function() {
+    this.media.muted = false;
+  };
+  
   export default MediaPlayer;
+  
